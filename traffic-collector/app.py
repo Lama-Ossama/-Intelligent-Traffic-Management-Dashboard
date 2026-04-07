fa2b9b3830846a7091382bc917c416ef7a471342
lama ossama <janaosama105@gmail.com>
2026-04-07 18:52:33 +0200

    Add traffic collector service with Dockerfile and sample data

---
+++ traffic-collector/app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import time
+
+#read the traffic data from the CSV file
+df = pd.read_csv('Traffic.csv')
+
+print("Starting Traffic Collector...")
+for index, row in df.iterrows():
+    # print the traffic data row by row as if it's coming from a sensor
+   print(
+        f"Time: {row['Time']} | Date: {row['Date']} | Day: {row['Day of the week']} | "
+        f"Cars: {row['CarCount']} | Bikes: {row['BikeCount']} | Buses: {row['BusCount']} | "
+        f"Trucks: {row['TruckCount']} | Total: {row['Total']} | Situation: {row['Traffic Situation']}"
+   )
+time.sleep(2) # sleep for 2 seconds between each row
